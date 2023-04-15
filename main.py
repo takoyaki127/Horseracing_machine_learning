11939aa5fdc1df6db89e30b6b9a69c02b46c014b
takoyaki127 <s826062@gmail.com>
2023-04-15 21:51:48 +0900

    moduleフォルダを生成

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from create_url import create_calender_URL
-from date_scraping import calender_to_event_date_link
-from race_scraping import day_to_race
+from module.create_url import create_calender_URL
+from module.date_scraping import calender_to_event_date_link
+from module.race_scraping import day_to_race
 import time
 
 # 開催日程のURLを生成   (year_start,year_end,month_start=1,month_end=12)
@@ -10,10 +10,12 @@ calender_url = create_calender_URL(2018, 2018, 11, 12)
 event_url = []
 for event_day in calender_url:
     event_url.extend(calender_to_event_date_link(event_day))
-    time.sleep(1)
+    time.sleep(0.5)
 
 # レースのURLを生成
 url_list = []
 for ele in event_url:
     url_list.extend(day_to_race(ele))
-    time.sleep(1)
+    time.sleep(0.2)
+
+pass
