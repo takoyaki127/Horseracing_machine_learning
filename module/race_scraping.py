d168b7ed9fe007b968f887d55cef860aed9cd1a8
takoyaki127 <s826062@gmail.com>
2023-04-15 22:49:49 +0900

    レースのURLではなく、IDを返すように変更

--- module/race_scraping.py
+++ module/race_scraping.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import re
 
 
 # 開催日からレースのURLをスクレイピング
 # chromeドライバを'./chromedriver_win32/chromedriver.exe'に入れてください。
 # ※　time.sleep()を使ってください。
-def day_to_race(url):
+def day_to_raceID(url):
     url = url
 
     options = webdriver.ChromeOptions()
@@ -20,4 +21,19 @@ def day_to_race(url):
         By.XPATH, "//dd[@class='RaceList_Data']/ul/li/a[@class='']")
     race_url = [x.get_attribute('href') for x in a_tags]
 
-    return race_url
+    race_ids = list(map(url_to_raceID, race_url))
+
+    return race_ids
+
+
+def url_to_raceID(url):
+    # s = 'https://race.netkeiba.com/race/result.html?race_id=201806050212&rf=race_list'
+    m = re.search(r'race_id=[0-9]*', url)
+    race_id = m.group()[8:]
+
+    return race_id
+
+
+# url = 'https://race.netkeiba.com/top/race_list.html?kaisai_date=20230401'
+# race_id = day_to_raceID(url)
+# pass
