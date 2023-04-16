b4e5493160cc21d43b52db71ee952100425d6861
takoyaki127 <s826062@gmail.com>
2023-04-16 21:46:03 +0900

    read_csvを関数化

--- read_csv.py
+++ read_csv.py
@@ -1,7 +1,14 @@
 import csv
 
-with open('./Race_URL.csv', 'r') as f:
-    render = csv.reader(f)
-    race_ids: list[str] = []
-    for x in render:
-        race_ids.extend(x)
+from scraping import scraping_result
+
+
+def read_csv(file_path):
+    # './race_id_(2023, 1)_(2023, 3).csv'
+    with open(file_path, 'r') as f:
+        render = csv.reader(f)
+        race_ids: list[str] = []
+        for x in render:
+            race_ids.extend(x)
+
+    return race_ids
