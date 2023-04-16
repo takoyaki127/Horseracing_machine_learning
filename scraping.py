677cfec94a721dcfbb78b8be758cb33afcbb274a
takoyaki127 <s826062@gmail.com>
2023-04-16 23:03:28 +0900

    修正

--- scraping.py
+++ scraping.py
@@ -8,30 +8,4 @@ def scraping_result(race_id):
     dfs = pd.read_html(url)
     df = dfs[0]
 
-    result = Result(df)
-    # for i in range(df.shape[0]):
-
-    #     rank = df["着 順"][i]
-    #     horse_name = df['馬名'][i]
-    #     jockey = df['騎手'][i]
-
-    #     flag = False
-    #     for ele in scores:
-    #         if ele.horse_name == horse_name and ele.jockey == jockey:
-    #             ele.add_score(rank)
-    #             # ele.count_up()
-    #             flag = True
-    #             break
-
-    #     if flag == False:
-    #         scores.append(Result(rank, horse_name, jockey))
-
-    return result
-
-
-# url = "https://db.netkeiba.com/race/202306010611/"
-# scores = []
-# scores = scraping_result(url, scores)
-
-# for ele in scores:
-#     print(ele.get_score())
+    return Result(df)
