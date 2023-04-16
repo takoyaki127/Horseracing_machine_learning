dcf31972f7cfd5ce3c2bcccfcda2e4a7f5afa50d
takoyaki127 <s826062@gmail.com>
2023-04-16 23:03:16 +0900

    list[Series]を作成

--- result.py
+++ result.py
@@ -4,5 +4,17 @@ from pandas import DataFrame
 class Result():
     def __init__(self, df: DataFrame):
         df = df
-        self.status = df.loc[:, ['馬名', '性齢']]
+        self.horse_num = df.shape[0]
+
+        # 属性を抽出
+        attribute = ['馬名', '性齢', '斤量', '騎手', 'タイム', '厩舎', '馬体重 (増減)']
+        selected_df = df.loc[:, attribute]
+
+        # 馬ごとのlist[Series]を生成
+        self.status = []
+        for i in range(self.horse_num):
+            self.status.append(selected_df.iloc[i,])
+
+        print(self.status[0])
+
         pass
