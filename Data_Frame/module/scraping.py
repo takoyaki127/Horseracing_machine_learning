b6f2e24d4fc5cded196574cef826d64993f4844a
takoyaki127 <s826062@gmail.com>
2023-04-18 23:46:23 +0900

    距離のSeriesを追加

--- Data_Frame/module/scraping.py
+++ Data_Frame/module/scraping.py
@@ -1,12 +1,25 @@
 import pandas as pd
 from pandas import DataFrame
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import re
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+
+driver = webdriver.Chrome(
+    '../chromedriver_win32/chromedriver.exe', options=options)
+driver.implicitly_wait(10)
 
 
 def scraping_result(race_id):
     url = f"https://race.netkeiba.com/race/result.html?race_id={race_id}&rf=race_list"
     dfs = pd.read_html(url)
+
+    mileage = serch_mileage(url)
     df = dfs[0]
-    df = add_mileage(df, 10)
+    df = add_mileage(df, mileage)
 
     return df
 
@@ -17,3 +30,16 @@ def add_mileage(df: DataFrame, mileage):
     df = pd.concat([df, mileage_series], axis='columns')
 
     return df
+
+
+def serch_mileage(url):
+
+    driver.get(url)
+
+    ele = driver.find_elements(By.XPATH, "//div[@class='RaceData01']")
+    infos = ele[0].text.split('/')
+    mileage = re.search(r'[0-9]+', infos[1])
+
+    # driver.close()
+
+    return int(mileage.group())
