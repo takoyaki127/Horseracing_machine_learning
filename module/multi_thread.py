7996fd514a4c81f6ff97cfabd13e35e06697c876
takoyaki127 <s826062@gmail.com>
2023-05-03 22:57:49 +0900

    引数を変更、コメントを追加

--- module/multi_thread.py
+++ module/multi_thread.py
@@ -2,29 +2,32 @@ from threading import Thread
 
 
 class MultiThread():
-    def __init__(self, func, file_list) -> None:
+    def __init__(self, target, args_list: list[tuple]) -> None:
         self.threads: list[Thread] = []
-        self.__create_threads(func, file_list)
-        print('created threads')
+        self.__target = target
+        self.__args_list = args_list
+        self.__create_threads(target, args_list)
 
-    def __create_threads(self, func, file_list):
-        for file in file_list:
-            self.threads.append(Thread(target=func, args=(file,)))
+    def __create_threads(self):
+        for args in self.__args_list:
+            self.threads.append(Thread(target=self.__target, args=args))
+        print('created threads')
 
     def start(self, thread_num=1):
         canContinue = True
         while canContinue:
             print('threads start')
-            canContinue = self.__multi_thread_start(thread_num)
+            canContinue = self.__multi_thread_start(
+                thread_num)  # 残りの処理がない場合Falseを返します
             print('threads finish')
 
     def __multi_thread_start(self, thread_num):
         unexecuted_threads = len(self.threads)  # 未実行のスレッド数
 
-        if unexecuted_threads <= 0:
+        if unexecuted_threads <= 0:  # 残りの処理がない場合
             return False
 
-        if unexecuted_threads < thread_num:
+        if unexecuted_threads < thread_num:  # 残りの処理がスレッド数より少ない場合
             self.__thread_start(unexecuted_threads)
             self.__thread_join(unexecuted_threads)
             return False
@@ -34,10 +37,10 @@ class MultiThread():
         self.threads = self.threads[thread_num:]  # 終了したスレッドを除外
         return True
 
-    def __thread_start(self, n):
+    def __thread_start(self, n):  # スレッドの0~(n-1)番目まで実行
         for i in range(n):
             self.threads[i].start()
 
-    def __thread_join(self, n):
+    def __thread_join(self, n):  # スレッドの0~(n-1)番目が完了するまで待機
         for i in range(n):
             self.threads[i].join()
